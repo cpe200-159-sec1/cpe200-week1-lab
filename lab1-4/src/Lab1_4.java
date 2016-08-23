3948a26c93c33e1f947d9bfc979c4f05b02f7f67
Chawasit Tengtrairatana <chawasit_teng@cmu.ac.th>
2016-08-23 14:46:41 +0700

    580610631 implement lab 4

--- lab1-4/src/Lab1_4.java
+++ lab1-4/src/Lab1_4.java
@@ -3,17 +3,18 @@
  */
 public class Lab1_4 {
 
-    public float findAverage(int[] inputs)
-    {
-        /*
-          Implement your code here
-        */
-        return 0.0f;
-    }
-
     public static void main(String[] args) {
         Lab1_4 l = new Lab1_4();
         int[] inputs = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.printf("Average value is %f\n", l.findAverage(inputs));
+    }
+
+    public float findAverage(int[] inputs) {
+        float sum = 0;
+        for (int number :
+                inputs) {
+            sum += number;
+        }
+        return sum / inputs.length;
     }
 }
