998dd2a8e719dff1cbd9470195a7ae321fe0a222
ood9842 <ood9842@hotmail.com>
2016-08-23 19:35:34 +0700

    580610629 edit lab

--- lab1-4/src/Lab1_4.java
+++ lab1-4/src/Lab1_4.java
@@ -5,15 +5,17 @@ public class Lab1_4 {
 
     public float findAverage(int[] inputs)
     {
-        /*
-          Implement your code here
-        */
-        return 0.0f;
+        int sum=0;
+       for(int n=0;n<inputs.length;n++)
+       {
+           sum += inputs[n];
+       }
+       return sum/inputs.length;
     }
 
     public static void main(String[] args) {
         Lab1_4 l = new Lab1_4();
         int[] inputs = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.printf("Average value is %f\n", l.findAverage(inputs));
+        System.out.printf("Average value is %.2f\n", l.findAverage(inputs));
     }
 }
