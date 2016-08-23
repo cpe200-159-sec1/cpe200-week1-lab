be548c21a65ef1ab323991b40891a11620bada90
Kawewut Chujit <kawewut.chu@gmail.com>
2016-08-23 14:18:34 +0700

    580610616

--- hw1-2/src/Hw1_2.java
+++ hw1-2/src/Hw1_2.java
@@ -2,18 +2,24 @@
  * Created by pruet on 11/8/2559.
  */
 public class Hw1_2 {
-    float gpaCalculation(int[] credit, int[] grade)
-    {
-        /**
-         * your code here
-         */
-        return 0.0f;
-    }
-
     public static void main(String[] args) {
         int[] credit = {3, 3, 3, 3, 2};
         int[] grade  = {2, 4, 4, 3, 2};
         Hw1_2 h = new Hw1_2();
         System.out.printf("Your GPA is %f\n", h.gpaCalculation(credit, grade));
+    }
+
+    float gpaCalculation(int[] credit, int[] grade) {
+        /*
+         * your code here
+         */
+        float gpa = 0;
+        float divi = 0;
+        for (int i = 0; i < credit.length; i++) {
+            gpa += (credit[i] * grade[i]);
+            divi += (credit[i]);
+        }
+
+        return gpa / divi;
     }
 }
