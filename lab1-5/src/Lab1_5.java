f19dad6270f76085671e3d2e20bcddc69f8207dc
chalitphol <chalitphol@gmail.com>
2016-08-23 15:41:05 +0700

    580610630 Finish Lab

--- lab1-5/src/Lab1_5.java
+++ lab1-5/src/Lab1_5.java
@@ -7,10 +7,13 @@ public class Lab1_5 {
 
     public int sumSquareDifference(int i)
     {
-        /**
-         * Your code here
-         */
-        return 0;
+        int sum1=0,sum2=0;
+        for(int j=1;j<=i;j++){
+            sum1 += j*j;
+            sum2 += j;
+        }
+        sum2 = sum2*sum2;
+        return sum2-sum1;
     }
 
     public static void main(String[] args) {
