2ae64565a2f1f169c3e403f582a5616f925e3ff8
Wiphob <wiphobkaoian@gmail.com>
2016-08-23 15:42:40 +0700

    580610677 Finish HW 1-2

--- hw1-2/src/Hw1_2.java
+++ hw1-2/src/Hw1_2.java
@@ -4,16 +4,21 @@
 public class Hw1_2 {
     float gpaCalculation(int[] credit, int[] grade)
     {
-        /**
-         * your code here
-         */
-        return 0.0f;
+        float GPA;
+        float sum=0;
+        float sumcredit=0;
+        for(int i=0;i<grade.length;i++) {
+            sum += grade[i] * credit[i];
+            sumcredit += credit[i];
+        }
+        GPA = sum/sumcredit;
+        return GPA;
     }
 
     public static void main(String[] args) {
         int[] credit = {3, 3, 3, 3, 2};
         int[] grade  = {2, 4, 4, 3, 2};
         Hw1_2 h = new Hw1_2();
-        System.out.printf("Your GPA is %f\n", h.gpaCalculation(credit, grade));
+        System.out.printf("Your GPA is %.2f\n", h.gpaCalculation(credit, grade));
     }
 }
