ab562ab22460966e2260007efb9050793a0e7617
kawewut <kawewut.chu@gmail.com>
2016-08-24 08:58:19 +0700

    test push เมื่อวาน push ผ่าน itetll ไม่ได้

--- hw1-2/src/Hw1_2.java
+++ hw1-2/src/Hw1_2.java
@@ -5,7 +5,7 @@ public class Hw1_2 {
     public static void main(String[] args) {
         int[] credit = {3, 3, 3, 3, 2};
         int[] grade  = {2, 4, 4, 3, 2};
-        Hw1_2 h = new Hw1_2();
+        
         System.out.printf("Your GPA is %f\n", h.gpaCalculation(credit, grade));
     }
 
