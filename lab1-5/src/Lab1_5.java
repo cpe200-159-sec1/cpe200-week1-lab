ee6b161f0a405f490c1135f17e1c393b002c2fe9
gluayz72 <watchara_phorung@cmu.ac.th>
2016-08-26 15:57:32 +0700

    Complete all lab

--- lab1-5/src/Lab1_5.java
+++ lab1-5/src/Lab1_5.java
@@ -7,10 +7,11 @@ public class Lab1_5 {
 
     public int sumSquareDifference(int i)
     {
-        /**
-         * Your code here
-         */
-        return 0;
+        int sum1=0,sum2=0;
+        for(int j=1;j<=i;j++) sum1+=j*j;
+        for(int j=1;j<=i;j++) sum2+=j;
+        sum2*=sum2;
+        return (sum1>sum2)?(sum1-sum2):(sum2-sum1);
     }
 
     public static void main(String[] args) {
